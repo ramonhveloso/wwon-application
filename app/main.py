85a910719814ee6df83898f7839b552141146ae7
ramonhveloso <ramon.veloso@inovebanco.com.br>
2024-10-03 16:22:07 -0400

    fix:

--- app/main.py
+++ app/main.py
@@ -5,14 +5,10 @@ from app.database.base import Base
 from app.database.session import engine
 
 app = FastAPI(
-    title="Boilerplate padrão back-end",
-    description="""Este boilerplate oferece uma estrutura inicial otimizada para 
-                    o desenvolvimento de APIs back-end robustas e escaláveis. 
-                    Com foco em boas práticas de arquitetura, inclui configurações 
-                    essenciais para autenticação, controle de erros, 
-                    testes automatizados. 
-                    Ideal para acelerar o desenvolvimento de projetos com FastAPI 
-                    e facilmente adaptável a diferentes contextos de aplicação.""",
+    title="Wwon Application",
+    description="""API REST em Python para classificar automaticamente as 
+                    avaliações de clientes sobre o serviço/produto/suporte 
+                    (exemplos em anexo) de uma empresa em Positiva, Negativa ou Neutra.""",
     version="0.1.0",
 )
 
